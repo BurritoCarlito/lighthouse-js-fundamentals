e6016073d6a8b9f64eb0dab746cc03286b828441
Carlo Tanjuaquio <carlotanjuaquio@gmail.com>
2022-03-07 08:43:59 +0000

    for practice

--- conditionals.js
+++ conditionals.js
@@ -29,3 +29,14 @@ if (!raining) {
   console.log("Leave your umbrella at home!");
 }
 
+
+const whichSchool = function (age) {
+
+  if (age < 13) {
+    return("Elementary School"); {
+} else if (age >= 13 || age <= 18) {
+    return("Secondary School"); {
+} else if (age > 18) {
+    return("Lighthouse Labs");
+}
+}
