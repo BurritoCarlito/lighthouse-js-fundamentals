76fe57043b09a6b3d8cc1924d06d07f07d4b6b6a
Carlo Tanjuaquio <carlotanjuaquio@gmail.com>
2022-09-07 07:18:59 +0000

    added methods for game, scoring, start and end

--- TwO-O-Player/gameClass.rb
+++ TwO-O-Player/gameClass.rb
@@ -5,15 +5,58 @@ class Game
   def initialize(player1, player2)
     @player1 = player1
     @player2 = player2
+    @current_player = player1
   end
 
-  def new_question(player)
-    number1 = rand(20)
-    number2 = rand(20)
-
-    puts "#{player.name}: What is #{number1} + #{number2} equal to?"
-    answer = gets.chomp.to_i
+  def change_of_player
+    @current_player = if @current_player == @player1
+      @player2
+    else
+      @player1
   end
 end
 
+  #logic to ask question
+  def generate_question
+    # generates the two random numbers for the question
+    number1 = rand(20)
+    number2 = rand(20)
+
+    question_answer = number1 + number2
+
+    # asks the question
+    question = "#{@current_player.name}: What is #{number1} + #{number2} equal to?"
+    puts question
+
+    print 'Answer: '
+    # gets the other player's input
+    answer = gets.chomp
+
+    # logic to answer question
+
+    if answer.to_i == question_answer.to_i
+      puts "YES! You are correct."
+      puts "P1: #{@player1.lives}/3 vs P2:#{@player2.lives}/3"
+    else 
+      puts "Seriousl? No."
+      @current_player.lives -= 1
+      puts "P1: #{@player1.lives}/3 vs P2:#{@player2.lives}/3"
+  end
+
+  change_of_player
+    puts "----- NEW TURN -----"
+  end
+
+  # to start the game 
+  def game_start 
+    puts "----- START GAME -----"
+
+    while @player1.lives > 0 && @player2.lives > 0
+    generate_question
+  end
+
+  puts "#{@current_player.name} wins with a score of #{@current_player.lives}/3"
+  puts "----- GAME OVER -----"
+  end
+end
   
--- TwO-O-Player/main.rb
+++ TwO-O-Player/main.rb
@@ -1,2 +1,9 @@
 require('./gameClass.rb')
 require('./playerClass.rb')
+
+player1 = Player.new('Player 1')
+player2 = Player.new('Player 2')
+
+game = Game.new(player1, player2)
+
+game.game_start
