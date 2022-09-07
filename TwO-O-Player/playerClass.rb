b0bd425c35889ffcf105ab0e7ff5aecaabc43d58
Carlo Tanjuaquio <carlotanjuaquio@gmail.com>
2022-09-07 05:00:44 +0000

    refactored playerClass to use attr_accessor

--- TwO-O-Player/playerClass.rb
+++ TwO-O-Player/playerClass.rb
@@ -1,17 +1,13 @@
 # This is the playerClass, each player has a name, and 3 lives at the start of the game
 
 class Player 
+  attr_accessor :name, :lives
   
   def initialize(name)
     @name = name
     @lives = 3
   end
-
-  def name
-    @name
-  end
-
-  def minusLife
-    @lives = @lives - 1
-  end
 end
+
+player1 = Player.new('Carlo')
+puts player1.name
