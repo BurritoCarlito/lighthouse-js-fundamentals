211322e1feb2ea6c0a460f2c48d4c17cef6e32a5
Carlo Tanjuaquio <carlotanjuaquio@gmail.com>
2022-09-07 04:57:40 +0000

    corrected placement of playerClass

--- TwO-O-Player/questionClass.rb
+++ TwO-O-Player/questionClass.rb
@@ -1,17 +1,1 @@
-# This is the playerClass, each player has a name, and 3 lives at the start of the game
 
-class Player 
-  
-  def initialize(name)
-    @name = name
-    @lives = 3
-  end
-
-  def name
-    @name
-  end
-
-  def minusLife
-    @lives = @lives - 1
-  end
-end
